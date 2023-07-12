f32fa5d430e1983b75ec30828c2bbfab2c48fccf
ZoneTwelve <zonetwelve159@gmail.com>
2023-07-12 10:52:19 +0800

    [update] create project

---
+++ ExampleApp/ExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleAppApp.swift
+//  ExampleApp
+//
+//  Created by Wilson on 2023/7/12.
+//
+
+import SwiftUI
+
+@main
+struct ExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
